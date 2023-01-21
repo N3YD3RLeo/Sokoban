39339c343cb0f904296162c86e35493d28280bce
N3YD3RLeo <102084116+N3YD3RLeo@users.noreply.github.com>
2023-01-21 15:37:08 +0100

    Etape 2 - Liste Tableau V2 (boucle)

--- src/SequenceTableau.java
+++ src/SequenceTableau.java
@@ -1,11 +1,14 @@
 public class SequenceTableau {
 
     int[] tableau;
+    private int taille_max;
+    private int deb;
     int nb_elements;
 
     SequenceTableau(int taille_max){
         tableau = new int[taille_max];
-        nb_elements = 0;
+        this.taille_max = taille_max;
+        this.nb_elements = 0;
     }
 
     public String toString(){
@@ -14,58 +17,54 @@ public class SequenceTableau {
             return "[]";
         }
 
-
         String res = "[";
 
-        for (int i = 0; i<nb_elements-1; i++){
-            res+= tableau[i] + ", ";
+        for (int i = deb; i<deb+nb_elements-1; i++){
+            res+= tableau[modulo(i)] + ", ";
         }
 
-        res += tableau[nb_elements-1] + "]";
+        res += tableau[modulo(deb+nb_elements)-1] + "]";
 
         return res;
     }
+
+    int atIndex(int index){
+        return tableau[modulo(deb+index)];
+    }
+
     void insereTete(int element){
         if (estVide()){
-            tableau[0] = element;
+            tableau[deb] = element;
             nb_elements = 1;
             return;
         }
-        try {
-            for (int i = nb_elements; i > 0; i--) {
-                tableau[i] = tableau[i - 1];
-            }
-            tableau[0] = element;
-            nb_elements += 1;
+        deb = modulo(deb-1);
+        tableau[deb] = element;
+        nb_elements += 1;
+    }
+
+    private int modulo(int index){
+        int res = index % taille_max;
+        if (res<0){
+            res+= taille_max;
         }
-        catch(ArrayIndexOutOfBoundsException exception){
-            System.out.println("Erreur : " + exception);
-            System.exit(1);
-        }
+        return res;
     }
 
     void insereQueue(int element){
-        try {
-            tableau[nb_elements] = element;
-            nb_elements += 1;
-        }
-        catch(ArrayIndexOutOfBoundsException exception){
-            System.out.println("Erreur : " + exception);
-            System.exit(1);
-        }
+        tableau[modulo(deb+nb_elements)] = element;
+        nb_elements += 1;
     }
 
     int extraitTete(){
-        int res = tableau[0];
-        for (int i = 1; i<nb_elements; i++){
-            tableau[i-1] = tableau[i];
-        }
+        int res = tableau[deb];
+        deb = modulo(deb+1);
 
         nb_elements--;
         return res;
     }
 
     boolean estVide(){
-        return nb_elements==0;
+        return nb_elements == 0;
     }
 }
